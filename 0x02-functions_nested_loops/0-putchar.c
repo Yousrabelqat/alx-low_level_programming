8e9cb4274c3249191fca5aa8e688b59dd2c1b81c
Yousrabelqat <youssrabelqat@gmail.com>
2023-03-21 09:39:17 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-{
-_putchar('_');
+{_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -14,5 +13,4 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);
-}	
+return (0) ; }
