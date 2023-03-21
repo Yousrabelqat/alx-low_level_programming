8501487878e44ff6a7e081ead3cb2f8127380044
Yousrabelqat <youssrabelqat@gmail.com>
2023-03-21 10:14:38 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,12 @@
  * print_alphabet - Make the alphabet
  * Return: void
  */
-void print_alphabet(void){
-	char c;
-	for (c = 'a'; c <= 'z'; c++){
-	_putchar(c); }
-	_putchar('\n'); }
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
