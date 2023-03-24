682bc0ab073ac8fe366228bde1b52a3e540b4975
Yousrabelqat <youssrabelqat@gmail.com>
2023-03-23 17:35:06 -0700

    Task 4

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,23 @@
 #include "main.h"
+
 /**
- * _isupper - Check if a letter is upper
- * @x: The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
-int _isupper(int x)
+int _isupper(int c)
 {
-if (x >= 65 && x <= 90)
-{
-return (1);
-}
-return (0);
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
 }
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0x04-more_functions_nested_loops
+Learning Functions and Loops in C
