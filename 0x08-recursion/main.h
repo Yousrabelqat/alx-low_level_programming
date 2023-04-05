d64c8908a718a2d3e9baf9e16a52be93d11935c0
Yousrabelqat <youssrabelqat@gmail.com>
2023-04-05 13:47:01 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,14 @@
-#ifndef MAIN_H 
+#ifndef MAIN_H
 #define MAIN_H
-void _puts_recursion(char *s) ;
-int _putchar (char c);
-void _print_rev_recursion (char *s) ;
-int strien_recursion (char *s);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int sqrt_recursion(int n);
-int is_prime_number (int n); 
-int is_palindrome (char *s);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 #endif
